99e52fcb0fc32c7609ae071bdfde13ab833f48cb
a1exsh <alex.shulgin@gmail.com>
2026-03-11 22:23:30 +0100

    In the beginning there was the Arcane Void
    Trust me, bro.

---
+++ arcanevoid.rkt
@@ -0,0 +1,110 @@
+#lang racket                            ; /base
+; TODO: 'require' racket/match
+
+(require sdl3)
+
+(define width 800)
+(define height 600)
+
+(define bx 100)
+(define by 100)
+(define vx 5)
+(define vy 5)
+(define px 400)
+(define py 560)
+(define dx 0)                           ; last paddle movement dx
+
+(define (reset!)
+  (set! bx 100)
+  (set! by 100)
+  (set! vx 5)
+  (set! vy 5))
+
+(reset!)
+
+(define (ball-sits?)
+  (= 0 vx vy))
+
+(define (launch-ball!)
+  (set! vx 5)
+  (set! vy -5))
+
+(define (move-paddle! x)
+  (set! dx (- x px))
+  (set! px x)
+  (if (ball-sits?)
+      (set! bx (+ bx dx))
+      #f))
+
+(define (handle! ev)
+  (match ev
+    [(quit-event) #t]
+    [(key-event 'down 'escape _ _ _) #t]
+    [(mouse-motion-event x _ _ _ _)
+     (move-paddle! x)
+     #f]
+    [(mouse-button-event 'down 'left _ _ _)
+     (if (ball-sits?)
+         (launch-ball!)
+         #f)
+     #f]
+    [_ #f]))
+
+(define (render-paddle! ren x y)
+  (set-draw-color! ren 255 255 255)
+  (fill-rect! ren (- x 30) (- y 5) 60 10))
+
+(define (render-ball! ren x y)
+  (set-draw-color! ren 200 0 0)
+  (fill-rect! ren (- x 5) (- y 5) 10 10))
+
+(define (render! ren)
+  (set-draw-color! ren 0 0 0)
+  (render-clear! ren)
+  (render-paddle! ren px py)
+  (render-ball! ren bx by))
+
+(define (move-ball!)
+  (set! bx (+ bx vx))
+  (set! by (+ by vy))
+  (cond
+    [(not (< 0 bx width))
+     (set! vx (- vx))]
+    
+    [(or (< by 0)
+         (and (< (- px 30) bx (+ px 30))
+              (< (- py 10) by (+ py))))
+     (set! vy (- vy))
+     (set! vx (+ vx (/ dx 2)))]
+
+    [(< height by)
+     (set! bx px)
+     (set! by (- py 5))
+     (set! vx 0)
+     (set! vy 0)]))
+
+(define (move!)
+  (if (ball-sits?)
+      #f
+      (move-ball!))
+
+  ; reset dx after each move so it doesn't stick
+  (set! dx 0))
+
+(define (game)
+  (with-sdl
+    (with-window+renderer "Arcane Void" width height (win ren)
+      (let loop ()
+        (define quit?
+          (for/or ([ev (in-events)])
+            (handle! ev)))
+        (move!)
+        (unless quit?
+          (render! ren)
+          (render-present! ren)
+          (delay! 16)
+          (loop))))))
+
+(if (equal? '#("-f") (current-command-line-arguments))
+    (game)
+    (thread game))
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+exec racket arcanevoid.rkt -f
